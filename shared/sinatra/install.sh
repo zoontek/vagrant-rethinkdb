4374666cb06ebfda9524580eb08cfda27c67bba6
zoontek <zoontek@gmail.com>
2015-04-22 02:53:37 +0200

    Add apt-get update

--- shared/sinatra/install.sh
+++ shared/sinatra/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-add-repository -y ppa:brightbox/ruby-ng
+sudo apt-get update
 sudo apt-get -y install ruby2.2 ruby-switch
 sudo ruby-switch --set ruby2.2
 sudo gem install sinatra rethinkdb
